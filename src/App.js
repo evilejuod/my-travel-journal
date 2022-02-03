8a6432b6b02d8fa3ab3df3117d3f076f23db4716
Evile <evile.juodziukynaite@gmail.com>
2022-02-03 12:27:08 +0200

    navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>hi</h1>
+      <Navbar />
     </div>
   );
 }
 
 export default App;
+ 
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import { FaGlobeEurope } from "react-icons/fa";
+
+function Navbar(){
+
+    return (
+        <nav>
+            <FaGlobeEurope size='24px' color='white'/>
+            <p>my travel journal.</p>
+        </nav>
+    )
+}
+export default Navbar;
